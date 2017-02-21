d16ba0e085f8fdbeea71485ab75924761646db91
Jac Stewart <stewartjks@gmail.com>
2017-02-20 20:50:17 -0800

    added listen for exclamations fcn

---
+++ scripts/first_test.js
@@ -0,0 +1,8 @@
+module.exports = function(jacbot) {
+  jacbot.hear(/!/, function(res) {
+    return res.reply("Calm down. Pls. Your excitement is inappropriate for a classroom environment. Pls.");
+  });
+  jacbot.hear(){
+    return
+  }
+}
