cd3a6c7029a9e441e02707f2d3db03f063771389
Jac Stewart <stewartjks@gmail.com>
2017-03-12 13:24:49 -0700

    added functions for naming favorite president and calculating days to Pi Day

--- scripts/first_test.js
+++ scripts/first_test.js
@@ -2,8 +2,8 @@
 Description:
 Provides functionality for Jacbot, a Slackbot who does three things:
   1. Asks users to calm down if they use more than three consecutive exclamation points.
-  2. Provides the Slack image of a team member when someone asks who that user is.
-  3. Tell the user which chapters of Eloquent JavaScript have been assigned so far.
+  2. Names her favorite president, randomly alternating among seven presidents.
+  3. Tells the user how many days are left until the next Pi Day.
 
 Dependencies:
   None
@@ -12,8 +12,7 @@ Configuration:
   None
 
 Commands:
- hubot <trigger> - <what the respond trigger does>
- <trigger> - <what the hear trigger does>
+ jacbot.hear( listens for a regular expression in user input and responds with a function)
 
 Notes:
 
@@ -24,38 +23,55 @@ stewartjks
 module.exports = function(jacbot) {
   jacbot.hear(/!!!!/, function(res) {
     return res.reply("Calm down. Pls. Your excitement is inappropriate for a classroom environment. Pls.");
-  })
-  jacbot.hear(/list Slack users/, function(res) {
-      return jacbot.http(`https://slack.com/api/users.list`)
-      .get('xoxp-132957885761-132974520752-145976002368-d25fd21987126404f0cb148687fedf45')
-      (function (err, res, body) {
-          try {
-            // let json = JSON.parse(body);
-            // return res.reply(json);
-            return res.reply("Hi");
-          } catch (error) {
-            return res.reply("Data not found.");
-          }
-      });
   });
-}
-
-/************************************
-EXAMPLES OF THE KEY HUBOT FUNCTIONS
-************************************/
-
-/*
-var fruits;
-fruits = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
-module.exports = function(robot) {
-  // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!"
-  return robot.respond(/hi|hello/i, function(msg) {
-    return msg.send("Howdy!");
-  });
-  // Random Example
-  //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
-  return robot.hear(/ship it/i, function(msg) {
-    return msg.send(msg.random(squirrels));
+  jacbot.hear(/who is your favorite (.*)/, function(res) {
+   var fav = res.match[1];
+   var presidents = ["Washington", "Polk", "Cleveland", "Kennedy", "Johnson", "Carter", "Reagan"];
+   var getRandomIntInclusive = function(min, max){
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random() * (max - min + 1)) + min;
+   };
+  //  Subtracting one from length of presidents array to account for zero-index
+   var randomPresidentIndex = getRandomIntInclusive(0, (presidents.length-1));
+   var randomPresident = presidents[randomPresidentIndex];
+   switch (fav) {
+     case "author":
+       return res.reply("Brendan Eich!");
+       break;
+     case "student":
+       return res.reply("I don't have a favorite " + fav + ". You're all wonderful.");
+       break;
+     case "president":
+     return res.reply("Right now, I'm a big " + randomPresident + " fan, but I change my mind pretty often.")
+       break;
+     default:
+       return res.reply("I don't know much about " + fav + ". What's your preferred " + fav + "?");
+   };
+ });
+ jacbot.hear(/how many days until Pi Day/, function(res) {
+   var singleDay = 24 * 60 * 60 * 1000;
+   var today = new Date();
+   var todayTime = today.getTime();
+   var currentYear = today.getFullYear();
+   var nextYear = currentYear + 1;
+   var currentYearPiDay = new Date("March 14, " + currentYear);
+   var nextYearPiDay = new Date ("March 14, " + nextYear);
+   var currentYearPiDayTime = currentYearPiDay.getTime();
+   var nextYearPiDayTime = nextYearPiDay.getTime();
+   var daysToCurrentPiDay = Math.round((currentYearPiDayTime - todayTime) / singleDay);
+   var daysToNextYearPiDay = Math.round((nextYearPiDayTime - todayTime) / singleDay);
+   if (daysToCurrentPiDay = 0) {
+      return res.reply("Today is Pi Day! WOO!");
+    }
+    else if (daysToCurrentPiDay = 1) {
+      return res.reply("There is " + daysToCurrentPiDay + " day remaining until Pi Day.");
+    }
+    else if (daysToCurrentPiDay > 0) {
+      return res.reply("There are " + daysToCurrentPiDay + " days remaining until Pi Day.");
+    }
+    else {
+      return res.reply("There are " + daysToNextYearPiDay + " days remaining until Pi Day.");
+    }
   });
 };
-*/
